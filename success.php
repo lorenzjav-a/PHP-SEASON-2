cd8b23c194a365c9d6d36def2b52482a5c23bb58
lorenz john delizo <delizola@students.nu-lipa.edu.ph>
2026-02-18 22:36:54 +0800

    final

--- success.php
+++ success.php
@@ -5,4 +5,4 @@
 ?>
 
 <h3>New Record Has Been Successfully Inserted! </h3>
-<h5> Check the records by clicking <a href='../ViewRecord.php'>here</a> </h5>
+<h5> Check the records by clicking <a href='ViewRecord.php'>here</a> </h5>
